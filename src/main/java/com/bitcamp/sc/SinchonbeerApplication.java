648814550bfea1f0d367374bb0cbf28ce1ab6bad
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-31 16:06:04 +0900

    Initial commit

---
+++ src/main/java/com/bitcamp/sc/SinchonbeerApplication.java
@@ -0,0 +1,14 @@
+package com.bitcamp.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class SinchonbeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinchonbeerApplication.class, args);
+    }
+
+}
