aab5045f7d0df1a0e57c22a46d95c7d57929f2ba
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2022-01-08 20:57:36 +0900

    feat: 상품 url 변경

---
+++ src/main/java/com/bitcamp/sc/domain/goods/domain/GoodsVO.java
@@ -0,0 +1,26 @@
+package com.bitcamp.sc.domain.goods.domain;
+
+import lombok.*;
+
+@Getter 
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GoodsVO {
+
+
+	private long idx; // 상품번호
+	private String name; // 상품이름
+	private int price; //상품가격
+	private String photo; //상품이미지
+	private String title; // 상품 상세 설명
+
+	@Builder
+	public GoodsVO(String name, int price, String photo, String title) {
+		this.name = name;
+		this.price = price;
+		this.photo = photo;
+		this.title = title;
+	}
+}
