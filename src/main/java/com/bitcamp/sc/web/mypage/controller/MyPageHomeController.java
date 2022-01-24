1b543947a1593c2a21b3efcdbbed197e3ca7d371
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2022-01-24 22:09:21 +0900

    feat: 마이페이지 홈 -> 상품 주문 내역 url 수정

--- src/main/java/com/bitcamp/sc/web/mypage/controller/MyPageHomeController.java
+++ src/main/java/com/bitcamp/sc/web/mypage/controller/MyPageHomeController.java
@@ -1,9 +1,13 @@
 package com.bitcamp.sc.web.mypage.controller;
 
+import com.bitcamp.sc.web.login.dto.LoginInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -12,7 +16,10 @@ public class MyPageHomeController {
 
 	// 주문 내역 페이지 이동
 	@GetMapping("/mypage/home")
-	public String mypageShop() {
-			return "mypage/myPageHome";
+	public String mypageShop(HttpSession session, Model model) {
+		LoginInfo loginMember = (LoginInfo) session.getAttribute("loginMember");
+		long idx = loginMember.getIdx();
+		model.addAttribute("idx",idx);
+		return "mypage/myPageHome";
 	}
 }
